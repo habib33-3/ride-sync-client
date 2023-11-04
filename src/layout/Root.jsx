8aed57532ca09cc91f7784e050193d5ebd8c06a9
Habibur Rahman <habib.swi16@gmail.com>
2023-11-05 00:35:50 +0600

    navbar created

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
-
-
+import Navbar from "../shared/Navbar/Navbar";
 
 const Root = () => {
-    return (
-        <Outlet/>
-    );
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 };
 
 export default Root;
