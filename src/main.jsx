9ecae6f15ca6ebc14c50b05caaa8685743a41b31
Habibur Rahman <habib.swi16@gmail.com>
2023-11-04 21:35:07 +0600

    basic project setup

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { router } from "./router/Router";
+import { HelmetProvider } from "react-helmet-async";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </React.StrictMode>
+);
