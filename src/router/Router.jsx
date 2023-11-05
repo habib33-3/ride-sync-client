00c660addab72b4b241e02069ec582b4b58bd355
Habibur Rahman <habib.swi16@gmail.com>
2023-11-05 16:33:28 +0600

    create user with email and password implemented

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,16 +2,22 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
 import Home from "../pages/Home/Home";
 import ErrorPage from "../Error/ErrorPage";
+import Register from "../pages/Register/Register";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement:<ErrorPage/>,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
         element: <Home />,
+      },
+
+      {
+        path: "/register",
+        element: <Register />,
       },
     ],
   },
